68017b0a2ea3db74176894245a43c890437d7569
Ming Jun <evan.zhangmingjun@gmail.com>
2024-06-07 22:05:13 +0800

    chore: automatic formatting by prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 function App() {
-
   return (
-    <div className="flex flex-col gap-4 place-content-center place-items-center w-screen h-screen">
-      <h1 className="text-3xl font-bold">Tailwind is working as intended <span className="underline">if this is underlined</span></h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="flex h-screen w-screen flex-col place-content-center place-items-center gap-4">
+      <h1 className="text-3xl font-bold">
+        Tailwind is working as intended{" "}
+        <span className="underline">if this is underlined</span>
+      </h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
     </div>
   );
 }
